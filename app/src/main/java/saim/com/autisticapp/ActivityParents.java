e276539fc14f09776ba163e913f099754faf1c68
Saim <streloy@gmail.com>
2019-08-16 23:13:42 +0600

    2019-08-16 11:13 PM

---
+++ app/src/main/java/saim/com/autisticapp/ActivityParents.java
@@ -0,0 +1,14 @@
+package saim.com.autisticapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityParents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parents);
+    }
+}

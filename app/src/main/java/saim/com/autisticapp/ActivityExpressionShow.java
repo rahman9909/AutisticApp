8ab1e5308b2297f649f87ac3708580f22b285fb2
Saim <streloy@gmail.com>
2019-09-13 19:26:50 +0600

    2019-09-13 07:08:00 PM

--- app/src/main/java/saim/com/autisticapp/ActivityExpressionShow.java
+++ app/src/main/java/saim/com/autisticapp/ActivityExpressionShow.java
@@ -1,16 +1,22 @@
 package saim.com.autisticapp;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class ActivityExpressionShow extends AppCompatActivity {
 
     String EXPRESSION = "";
+    private TextToSpeech textToSpeech;
 
-    ImageView imgExpression;
+    ImageView imgExpression, imgExpressionSound;
     TextView txtExpression;
 
 
@@ -27,7 +33,16 @@ public class ActivityExpressionShow extends AppCompatActivity {
         this.EXPRESSION = getIntent().getExtras().getString("EXPRESSION");
 
         imgExpression = (ImageView) findViewById(R.id.imgExpression);
+        imgExpressionSound = (ImageView) findViewById(R.id.imgExpressionSound);
         txtExpression = (TextView) findViewById(R.id.txtExpression);
+
+
+        imgExpressionSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Speakout(EXPRESSION);
+            }
+        });
 
         setAction();
     }
@@ -58,5 +73,24 @@ public class ActivityExpressionShow extends AppCompatActivity {
             imgExpression.setImageResource(R.drawable.ic_distuested);
             txtExpression.setText(EXPRESSION);
         }
+
+        Speakout(EXPRESSION);
+    }
+
+    public void Speakout(final String stringVoice) {
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = textToSpeech.setLanguage(Locale.US);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    } else {
+                        textToSpeech.speak(stringVoice, TextToSpeech.QUEUE_FLUSH, null);
+                    }
+                } else {
+                    Log.e("TTS", "Initilization Failed!");
+                }
+            }
+        });
     }
 }
